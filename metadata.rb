039f11d769783d79099c1157380b6ba3c47e93e9
Tim Smith <tsmith@chef.io>
2016-09-03 00:38:02 -0700

    Resolve cookstyle warnings
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -5,8 +5,8 @@ license           'Apache 2.0'
 description       'Installs Microsoft .NET 4.0'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '2.0.0'
-supports          'windows'
+supports 'windows'
 
 source_url 'https://github.com/chef-cookbooks/ms_dotnet4'
 issues_url 'https://github.com/chef-cookbooks/ms_dotnet4/issues'
-chef_version     '>= 12.1' if respond_to?(:chef_version)
+chef_version '>= 12.1' if respond_to?(:chef_version)
